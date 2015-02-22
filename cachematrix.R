10b3a865c28f29538bb8908a207f952f62f8341f
unknown <bromeroo@yahoo.es>
2015-02-22 16:20:16 +0100

    Caching inverses

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,55 @@
 ## functions do
 
 ## Write a short comment describing this function
-
 makeCacheMatrix <- function(x = matrix()) {
-
+      inverse <- NULL
+      set <- function(y) {
+            x <<- y
+            inverse <<- NULL
+      }
+      get <- function() x
+      setinverse <- function(inverse_param) inverse <<- inverse_param
+      getinverse <- function() inverse
+      list(set = set, get = get,
+           setinverse = setinverse,
+           getinverse = getinverse)
 }
-
-
-## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+      inverse <- x$getinverse()
+      if(!is.null(inverse)) {
+            message("getting cached data")
+            return(inverse)
+      }
+      data <- x$get()
+      inverse <- solve(data, ...)
+      x$setinverse(inverse)
+      inverse
 }
+
+# Examples:
+# > m <- makeCacheMatrix()
+# > m$set(matrix(c(1,1,1,3,4,3,3,3,4), 3, 3))
+# > m$get()
+# [,1] [,2] [,3]
+# [1,]    1    3    3
+# [2,]    1    4    3
+# [3,]    1    3    4
+# > mi <- <- cacheSolve(m)
+# > mi
+# [,1] [,2] [,3]
+# [1,]    7   -3   -3
+# [2,]   -1    1    0
+# [3,]   -1    0    1
+# > mi <- cacheSolve(m)
+# getting cached data
+# > mi
+# [,1] [,2] [,3]
+# [1,]    7   -3   -3
+# [2,]   -1    1    0
+# [3,]   -1    0    1
+# > m$get() %*% mi
+# [,1] [,2] [,3]
+# [1,]    1    0    0
+# [2,]    0    1    0
+# [3,]    0    0    1
